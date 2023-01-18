24a12d3a0daf14f9c68f1a5aa8b0d84058bbc33b
Steven <xkao1@DESKTOP-OTN563I>
2023-01-18 13:28:04 +0800

    lock add

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Test");
+    println!("請猜測一個數字!");
+    println!("請輸入你猜的數字:");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("讀取該行失敗!");
+
+    println!("你猜測的數字: {guess}");
 }
